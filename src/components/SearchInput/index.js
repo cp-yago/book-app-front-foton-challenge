17fe436e87b49650fe6e55c73e56f9ae652393d9
cp-yago <yago.cunha123@gmail.com>
2021-08-28 13:19:33 -0300

    book search

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -4,12 +4,13 @@ import { SearchInput, Container } from './styles'
 
 import SearchIcon from '../../assets/icons/search.svg'
 
-const Search = () => {
+const Search = ({ onChange }) => {
   return (
     <Container>
       <img src={SearchIcon} alt="search-icon"/>
       <SearchInput 
         placeholder="Search book"
+        onChange={(e) => onChange(e.target.value)}
       />
     </Container>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react'
+import React, { useEffect, useCallback, useState, useMemo } from 'react'
 
 import { Container } from './styles'
 
@@ -10,14 +10,30 @@ import BookList from '../../components/BookList'
 import api from '../../services/api'
 
 const Home = () => {
+
+  const initialFilters = useMemo(() => ({
+    page: 0,
+    pageLimit: 5,
+  }), [])
+
+  const [filters, setFilters] = useState({ ...initialFilters })
   const [books, setBooks] = useState([])
 
-  const getBooks = useCallback(async () => {
-    const response = await api.get("/books")
+  const getBooks = useCallback(async (query = {}) => {
+    const params = {
+      ...filters,
+      ...query
+    }
+
+    const response = await api.get("/books", { params })
     if (response) {
       setBooks(response.data)
     }
-  }, [])
+  }, [filters])
+
+  const handleSearchInputChange = useCallback((value) => {
+    getBooks({ name: value })
+  }, [getBooks])
 
   useEffect(() => {
     getBooks()
@@ -26,7 +42,7 @@ const Home = () => {
   return (
     <Container>
 
-      <SearchInput />
+      <SearchInput onChange={handleSearchInputChange} />
 
       <WelcomeHeader userName="Yago Cunha"/>
 
